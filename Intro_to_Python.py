282a86de4c40902a2e40cf82761c0fe527d2bffd
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-09-20 20:42:13 +0300

    Introduction to Python
    A brief introduction to python programming language

---
+++ Intro_to_Python.py
@@ -0,0 +1,15 @@
+#_INTRODUCTION TO PYTHON_
+
+# Python is a high-level, general-purpose and powerful programming language.
+# It is widely used for web development, data analysis, machine learning, automation, scientific computing, game development and Internet Of Things.
+# It was created by a Dutch programmer, Guido van Rossum and released in 1991. 
+# It has gained widespread popularity due to its versatility and extensive range of applications.
+
+# Python's key features include:
+# Easy to learn and read. Python syntax is designed to be readable and easy to understand 
+# Versatile. Python offers a vast pool of libraries and modules
+# Cross-platform capability. Python runs on different operating systems
+# No compilation required. It is interpreted and executed line by line
+# Strong community support. Python has a large network of developers
+
+# Python syntax is simple, readable and easy to understand, making it suitable for beginners.
