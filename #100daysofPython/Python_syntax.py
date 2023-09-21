487f69c41b69f899ec6a758fecb22d644da14200
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-09-21 21:34:35 +0300

    Python_Syntax
    I have given an introduction to the python syntax, describes the basic components of the python syntax

---
+++ #100daysofPython/Python_syntax.py
@@ -0,0 +1,38 @@
+# _BASICS OF PYTHON SYNTAX_
+
+# Python syntax is simple, readable and easy to understand, making it suitable for beginners. It is composed of the following.
+# 1. Comments: Hash symbol (#) is used to write comments in Python. These are ignored by the interprter thus just used for documentation.
+# 2. Variables: Variables in Python do not require specific declarations. Values are assigned to variables using the assignment operator (=) ie name = "Joseph", age = 21
+# 3. Print Statement: The print() function is used to display output on the console for example print("Hello, World!")
+# 4. Basic Data Types: Python supports various data types, including integers(x=4), floating-point numbers(y=6.13), strings(name="Joseph"), booleans(is_Student=False), and more.
+# 5. Control Flow: Python supports control flow structures like if statements and loops.
+
+#- If statement:
+
+#if condition:
+    # code to execute if condition is True
+#else:
+    # code to execute if condition is False
+
+#- For loop:
+
+#for item in iterable:
+    # code to execute for each item in the iterable
+
+#- While loop:
+
+#while condition:
+    # code to execute while the condition is True
+# 6. Functions: Functions in Python allow you to group and reuse blocks of code. def is used to define that a specific block of code is a function.
+
+#def multiply(a, b):
+#    return a * b
+# 7. Indentation: Python makes use of indentation to denote blocks of code.
+
+#if condition:
+    # Code block 1
+#    statements
+#else:
+    # Code block 2
+#    statements
+
