5868c4606a62f0dc5e7b90d1004fc6e1c29a5468
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-09-23 19:16:35 +0300

    Alice's Grades
    A program that displays Alice's grades for the three assignments and their average

---
+++ Alice_Grade.py
@@ -0,0 +1,13 @@
+name = "ALICE"
+print(name)
+
+Grade_1, Grade_2, Grade_3  = 85, 90, 88
+Total_Grades = (Grade_1 + Grade_2 + Grade_3)
+print("Assignment 1 = " +str(Grade_1))
+print("Assignment 2 = " +str(Grade_2))
+print("Assignment 3 = " +str(Grade_3))
+
+
+Average_Grade = ((Total_Grades)/3)
+print("Alice's Average Grades = " +str(Average_Grade))
+
