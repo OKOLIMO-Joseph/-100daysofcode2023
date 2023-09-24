9ed2cb96a8aa1ae23d2317ad06bfd1b20933fa8f
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-09-24 20:50:23 +0300

    Variables.py
    Python Variables and their data types

---
+++ Variables.py
@@ -0,0 +1,27 @@
+print("VARIABLES AND THEIR DATA TYPES")
+
+my_int = 20
+print(my_int)
+print(type(my_int))
+
+my_float = 2.05
+print(my_float)
+print(type(my_float))
+
+my_string = "Okolimo"
+print(my_string)
+print(type(my_string))
+
+my_list = ['Python', 'Java', 'HTML']
+x, y, z = my_list
+print(my_list)
+print(type(my_list))
+
+my_tuple = ([52, 'BSIT'], [23, 'DEIT'])
+print(my_tuple)
+print(type(my_tuple))
+
+my_dict = {"Uganda" : "Kampala", "Rwanda" : "Kigali", "Nigeria" : "Lagos"}
+print(my_dict)
+print(type(my_dict))
+
