30c0a673478dd99ad1c49261b00a03962816bd3e
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-09-25 18:40:22 +0300

    User_Input.py
    A program that asks for the user name(string), age(integer) and body temperature(float) and displays them as a formatted message

---
+++ User_Input.py
@@ -0,0 +1,15 @@
+#Asking for user input
+name = str(input("What is your name? "))
+
+age = int(input("How old are you?  "))
+
+temp = float(input("What is your body temperature? "))
+
+#Displaying the inputs
+print("--------------------------")
+print(" -BELOW ARE YOUR DETAILS-    ")
+print("--------------------------")
+print("NAME : " +str(name))
+print("AGE : " +str(age))
+print("BODY TEMPERATURE : " +str(temp))
+print("--------------------------")
