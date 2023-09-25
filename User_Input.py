1580fc73954fe89473bd2f6b6317c451a54335bb
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-09-25 20:09:30 +0300

    user_input.py
    A program that asks for a users details and displays them as a formatted message

--- User_Input.py
+++ User_Input.py
@@ -3,7 +3,11 @@ name = str(input("What is your name? "))
 
 age = int(input("How old are you?  "))
 
-temp = float(input("What is your body temperature? "))
+reg_no = str(input("What is your Registration Number? "))
+
+course = str(input("Which course do you do? "))
+
+CGPA = float(input("What is your CGPA? "))
 
 #Displaying the inputs
 print("--------------------------")
@@ -11,5 +15,7 @@ print(" -BELOW ARE YOUR DETAILS-    ")
 print("--------------------------")
 print("NAME : " +str(name))
 print("AGE : " +str(age))
-print("BODY TEMPERATURE : " +str(temp))
+print("REG. NO. : " +str(reg_no))
+print("COURSE : " +str(course))
+print("CGPA : " +str(temp))
 print("--------------------------")
