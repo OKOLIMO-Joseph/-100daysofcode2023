7f415cdf2dc84ad5ddd6e1f48a70974785114133
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-10-04 22:11:48 +0300

    Day-3.py
    A program that prints out the product of integers and floats

---
+++ Day-3.py
@@ -0,0 +1,12 @@
+#Function to get the product of 5 numbers
+#I first created the function "def multiply"
+# a is the variable containing the  product of the multiplication
+#I then print the product as a string
+
+def multiply():
+
+    a = (3*8*4*12.5*5.767)
+    
+    print("Product = " +str(a))
+
+multiply()
